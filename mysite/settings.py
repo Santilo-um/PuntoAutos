140109a5f0cccd48c2159c0e81b8b79c3fbfa258
Santilo-um <slongo@alumno.edu.ar>
2025-11-17 22:35:26 -0300

    database cambiada

--- mysite/settings.py
+++ mysite/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.parse(os.getenv("DATABASE_URL"), conn_max_age=600)
+    "default": dj_database_url.parse(os.getenv("DATABASE_URL", "sqlite:///db.sqlite3"), conn_max_age=600)
 }
 
 # Password validation
